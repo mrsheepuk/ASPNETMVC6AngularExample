92199376fcbcc258b7be0883ccdbd711f51f3b9d
mrsheepuk <mark@mrsheep.co.uk>
2015-09-29 17:28:46 +0100

    Add routes file with no routes in.

---
+++ src/AngularGettingStarted/wwwroot/js/src/routes.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app').config(configureRoutes);
+
+    configureRoutes.$inject =
+      ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+
+    function configureRoutes($stateProvider, $urlRouterProvider,
+      $locationProvider) {
+
+        // Routes go here
+
+        $urlRouterProvider.otherwise('/');
+
+        $locationProvider.html5Mode(true);
+    }
+
+})();
