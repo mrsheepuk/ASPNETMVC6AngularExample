3898c38561fc13897675d7390a919392c28b4011
mrsheepuk <mark@mrsheep.co.uk>
2015-09-29 12:26:03 +0100

    Add a class to represent environmental info into the views.

---
+++ src/AngularGettingStarted/Models/EnvInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularGettingStarted.Models
+{
+    public class EnvInfo
+    {
+        public bool Development { get; set; }
+        public bool Production { get; set; }
+        public string Version { get; set; }
+    }
+}
