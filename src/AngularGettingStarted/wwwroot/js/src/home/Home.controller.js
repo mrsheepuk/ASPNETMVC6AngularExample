2f8c4718688355258e22df4310c14edeb2d45b2b
mrsheepuk <mark@mrsheep.co.uk>
2015-10-02 15:44:23 +0100

    Load our thingList from the server.

--- src/AngularGettingStarted/wwwroot/js/src/home/Home.controller.js
+++ src/AngularGettingStarted/wwwroot/js/src/home/Home.controller.js
@@ -5,14 +5,14 @@
 
     // Inject the services and data this controller depends on - this is how we wire 
     // services to controllers.
-    Home.$inject = ["thingList"];
+    Home.$inject = ["thingListService"];
 
     // Define the controller - the parameters passed into this function will be 
     // auto-provided by angular based on the $inject line above.
-    function Home(thingList) {
+    function Home(thingListService) {
         var vm = this;
         // "Public" properties
-        vm.list = ["Example String 1", "A second Thing!"];
+        vm.list = [];
         vm.newItem = "";
 
         // "Public" functions
@@ -22,6 +22,7 @@
         // "Private" properties
 
         // Initialisation
+        refreshList();
 
         // "Public" function definitions
         function addItem() {
@@ -43,6 +44,14 @@
         }
 
         // "Private" function definitions
+        function refreshList() {
+            return thingListService.getThings().then(function (things) {
+                vm.list.length = 0;
+                // Add each thing to our local array.
+                for (var x = 0; x < things.length; x++)
+                    vm.list.push(things[x]);;
+            });
+        }
 
         // Event Subscriptions
     }
