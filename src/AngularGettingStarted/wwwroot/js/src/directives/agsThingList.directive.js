0c8d9102f1c76eb35a9301c8e2250bf327e61e29
mrsheepuk <mark@mrsheep.co.uk>
2015-10-05 09:37:17 +0100

    Add empty directive, with controller, link function and template.

---
+++ src/AngularGettingStarted/wwwroot/js/src/directives/agsThingList.directive.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    'use strict';
+    // Registers this directive with the Angular module "app"
+    angular.module('app').directive('agsThingList', agsThingList);
+
+    function agsThingList() {
+        var directive = {
+            link: link,
+            templateUrl: "",
+            restrict: "EA",
+            controller: agsThingListController,
+            controllerAs: "agsThingList"
+        }
+
+        function link(scope, el, attr, ctrl) {
+
+        }
+    }
+
+    // Inject the services and data this controller depends on - this is how we wire 
+    // services to controllers.
+    agsThingListController.$inject = [];
+
+    function agsThingListController() {
+        var vm = this;
+        // "Public" properties
+
+        // "Public" functions
+
+        // "Private" properties
+
+        // Initialisation
+
+        // "Public" function definitions
+
+        // "Private" function definitions
+
+        // Event Subscriptions
+    }
+})();
