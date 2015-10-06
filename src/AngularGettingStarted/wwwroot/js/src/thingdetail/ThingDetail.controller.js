7b40d8d43c8958c6995234d7d27fc55b6eded848
mrsheepuk <mark@mrsheep.co.uk>
2015-10-06 16:59:34 +0100

    Thing detail editing.

--- src/AngularGettingStarted/wwwroot/js/src/thingdetail/ThingDetail.controller.js
+++ src/AngularGettingStarted/wwwroot/js/src/thingdetail/ThingDetail.controller.js
@@ -2,20 +2,40 @@
     'use strict';
     angular.module('app').controller('ThingDetail', ThingDetail);
 
-    ThingDetail.$inject = ["thing"];
+    ThingDetail.$inject = ["thing", "thingListService"];
 
-    function ThingDetail(thing) {
+    function ThingDetail(thing, thingListService) {
         var vm = this;
         // "Public" properties
         vm.thing = thing;
+        vm.saving = false;
+        vm.errMsg = null;
 
         // "Public" functions
+        vm.save = save;
 
         // "Private" properties
 
         // Initialisation
 
         // "Public" function definitions
+        function save() {
+            vm.saving = true;
+            thingListService.updateThing(vm.thing.Value, vm.thing).then(
+                function (updatedThing) {
+                    // In case the server has changed anything, 
+                    // update our local thing (e.g. if we had an "updatedTime"
+                    // property managed by the server).
+                    vm.thing = updatedThing;
+                    vm.saving = false;
+                },
+                // Catch any errors and display a message.
+                function (err) {
+                    vm.saving = false;
+                    vm.errMsg = err.message;
+                }
+            );
+        }
 
         // "Private" function definitions
 
