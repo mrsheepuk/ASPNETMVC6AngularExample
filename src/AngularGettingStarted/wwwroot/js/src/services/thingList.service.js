9a00f8f6317f20b2b7d823b10f434fd22b853b80
mrsheepuk <mark@mrsheep.co.uk>
2015-10-06 15:09:02 +0100

    Update the service to match our new API.

--- src/AngularGettingStarted/wwwroot/js/src/services/thingList.service.js
+++ src/AngularGettingStarted/wwwroot/js/src/services/thingList.service.js
@@ -15,6 +15,8 @@
         var service = {
             getThings: getThings,
             addThing: addThing,
+            getThing: getThing,
+            updateThing: updateThing,
             removeThing: removeThing
         };
 
@@ -30,15 +32,70 @@
         }
 
         function addThing(thing) {
-            return $http.post("/api/thinglist", { "Value": thing }).then(function (response) {
-                return response.data;
-            });
+            return $http.post("/api/thinglist", { "Value": thing }).then(
+                function (response) {
+                    return response.data;
+                },
+                function (errResponse) {
+                    if (response.status === 409) {
+                        throw new Error("AlreadyExists");
+                    } else {
+                        // Re-throw for default handling.
+                        throw errResponse;
+                    }
+                }
+            );
+        }
+
+        function getThing(thing) {
+            return $http.get("/api/thinglist" + thing).then(
+                function (response) {
+                    return response.data;
+                },
+                function (errResponse) {
+                    // If this was a not found, simply return null.
+                    if (response.status === 404) {
+                        return null;
+                    } else {
+                        // Re-throw for default handling.
+                        throw errResponse;
+                    }
+                }
+            );
+        }
+
+        function updateThing(thing, thingWithDetails) {
+            return $http.put("/api/thinglist" + thing, thingWithDetails).then(
+                function (response) {
+                    return response.data;
+                },
+                function (errResponse) {
+                    // If this was a not found, throw a specific error.
+                    if (response.status === 404) {
+                        throw new Error("DoesntExist");
+                    } else {
+                        // Re-throw for default handling.
+                        throw errResponse;
+                    }
+                }
+            );
         }
 
         function removeThing(thing) {
-            return $http.delete("/api/thinglist/" + thing).then(function (response) {
-                return response.data;
-            });
+            return $http.delete("/api/thinglist/" + thing).then(
+                function (response) {
+                    return response.data;
+                }, 
+                function (errReponse) {
+                    // If this was a not found, throw a specific error.
+                    if (response.status === 404) {
+                        throw new Error("DoesntExist");
+                    } else {
+                        // Re-throw for default handling.
+                        throw errResponse;
+                    }
+                }
+            );
         }
 }
 })();
